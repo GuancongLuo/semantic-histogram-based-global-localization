5c2dd80da8f30010431f7174a902933fc897f707
Clarence Luo <guancong.luo@dorabot.com>
2021-11-15 20:44:23 +0800

    change gatherDenseMap type

--- src/main.cpp
+++ src/main.cpp
@@ -137,7 +137,7 @@ int main(int argc, const char * argv[])
     pointCloudMapping pointCloudMapping;
     pointCloudMapping.pointVisuallize(cloud1, cloud2, inlierID, R, T);
 
-    int type = 1;
-    gatherDenseMap(cloud1,centerpoint1,pose1,Label,camera,scale,dir1,fileNumber1,type);
+    // int type = 1;
+    gatherDenseMap(cloud1,centerpoint1,pose1,Label,camera,scale,dir1,fileNumber1,0);
     
 }
